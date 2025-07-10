eb21fdb93f5dc3026c99057dcaf727f553bce133
gllun <gaelluntala@gmail.com>
2025-07-11 00:37:09 +0200

    fix add page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path' // Importation du module 'path' de Node.js
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        // On liste ici chaque page comme un point d'entrée
+        main: resolve(__dirname, 'index.html'),
+        produit: resolve(__dirname, 'produit.html'),
+        liste: resolve(__dirname, 'liste.html'),
+        // Ajoute ici tes futures pages de la même manière
+        // nom_unique: resolve(__dirname, 'autre-page.html'),
+      },
+    },
+  },
 })
